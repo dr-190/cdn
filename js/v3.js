26e169cb6094cdd3bd1d079cde2304eb79123b54
dr-190 <157780926+dr-190@users.noreply.github.com>
2024-01-26 16:21:56 +0800

    更新AP链接

--- js/v3.js
+++ js/v3.js
@@ -1,3 +1,3 @@
 var windowWidth = $(window).width();
-var meting_api = 'http://api.mizore.cn/meting/api.php?server=:server&type=:type&id=:id';
+var meting_api = 'https://api.injahow.cn/meting/?server=netease&type=playlist&id=:id';
 console.log('\n' + ' %c Music Player by Simeng ' + ' %c 音乐播放器 ' + '\n' + '\n', 'color: #fff; background: #fd79a8; padding:5px 0;', 'background: #FFF; padding:5px 0;');
